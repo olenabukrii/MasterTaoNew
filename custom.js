5d4307090423cec19f3edcd464ecdba80dad1648
Olena Bukrii <135062132+olenabukrii@users.noreply.github.com>
2023-11-02 16:14:17 +0100

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+
+	$('.first-screen-slider').slick({
+		dots: true,
+		infinite: true,
+		appendArrows: '.first-screen-slider-arrows',
+		responsive: [{
+				breakpoint: 992,
+				settings: {
+					arrows: false,
+				}
+			},
+  
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		]
+	});
+  
+	mobileOnlySlider("#advantages-slider", true, false, 991);
+	function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+	var slider = $($slidername);
+	var settings = {
+		mobileFirst: true,
+		dots: $dots,
+		arrows: $arrows,
+		responsive: [
+		{
+			breakpoint: $breakpoint,
+			settings: "unslick"
+		}
+		]
+	};
+  
+	slider.slick(settings);
+  
+	$(window).on("resize", function () {
+		if ($(window).width() > $breakpoint) {
+		return;
+		}
+		if (!slider.hasClass("slick-initialized")) {
+		return slider.slick(settings);
+		}
+	});
+	} // Mobile Only Slider
+
+	$('.slider-template').slick({
+		dots: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+		  {
+			breakpoint: 1310,
+			settings: {
+			  slidesToShow: 2,
+			}
+		  },
+		  {
+			breakpoint: 890,
+			settings: {
+				slidesToShow: 1,
+				dots: true,
+				arrows: false,
+			}
+		  },
+		  {
+			breakpoint: 530,
+			settings: {
+			  slidesToShow: 1,
+			  dots: true,
+			  arrows: false,
+			}
+		  },
+		]
+	  });
+  
+});
+  
+const checkbox = document.querySelectorAll('[type="checkbox"]')
+
+
+console.log(document.querySelectorAll('[type="checkbox"]'));
+
+checkbox.forEach(item => {
+  item.closest('.input-box').classList.add('has-checkbox')
+  item.closest('.input-column').classList.add('padding')
+})
+
+document.getElementById('myfile').onchange = function() {
+
+  //long name
+  const fileName = document.getElementById('file-name');
+  fileName.innerHTML = this.value;
+  //short name
+  // document.getElementById('file-name').innerHTML = this.files[0].name;
+
+};
+  
